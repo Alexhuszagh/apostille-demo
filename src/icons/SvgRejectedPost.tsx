5de7b8e59370378af29a7aee078b4a7201ce7a98
Alex Huszagh <ahuszagh@ahuszagh.localdomain>
2019-05-28 15:59:44 -0500

    Added SVG images for verified and rejected icons.

---
+++ src/icons/SvgRejectedPost.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import SvgIcon from '@material-ui/core/SvgIcon'
+
+const SvgRejectedPost = (props: any) =>
+    <SvgIcon {...props} viewBox='0 0 15 24'><path d='M12.2692 2.7308c-2.6326-2.6326-6.906-2.6326-9.5384 0s-2.6326 6.906 0 9.5384 6.906 2.6326 9.5384 0 2.6326-6.906 0-9.5384h0ZM9.4077 10.3615L7.5 8.454l-1.9077 1.9077-0.9538-0.954L6.5461 7.5l-1.9077-1.9077 0.9539-0.9538L7.5 6.5461l1.9077-1.9077 0.954 0.9539L8.4538 7.5l1.9077 1.9077-0.954 0.954h0Z' /></SvgIcon>
+
+export default SvgRejectedPost
---
+++ src/icons/SvgVerifiedPost.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import SvgIcon from '@material-ui/core/SvgIcon'
+
+const SvgVerifiedPost = (props: any) =>
+    <SvgIcon {...props} viewBox='0 0 15 24'><path d='M7.5 10.25C4.05 10.25 1.25 13.05 1.25 16.5s2.8 6.25 6.25 6.25 6.25-2.8 6.25-6.25S10.95 10.25 7.5 10.25z m-1.25 9.375l-3.125-3.125 0.8812-0.8812L6.25 17.8562l4.7438-4.7437L11.875 14l-5.625 5.625z' /></SvgIcon>
+
+export default SvgVerifiedPost
